ca9921ae65ba22a47f4a7a814c72f93e4a8f8422
Shahina-2810 <shahina1028@gmail.com>
2024-09-27 08:20:42 +0530

    initial commit

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useEffect, useState } from 'react';
 import ReactMapGL, { Marker } from 'react-map-gl';
 import { RiUserLocationFill } from 'react-icons/ri';
@@ -45,3 +46,52 @@ const Map = ({ lat, lon }) => {
 };
 
 export default Map;
+=======
+import React, { useEffect, useState } from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
+import { RiUserLocationFill } from 'react-icons/ri';
+
+const API_KEY = 'pk.eyJ1Ijoic2hhaGluYS0yODEwIiwiYSI6ImNtMWJ0bzFqOTI3NnYya3I0YzR0MXQ0aHAifQ.cPNOuILUp1aBbiYlLdxkvA';
+
+const Map = ({ lat, lon }) => {
+  // Initial viewport settings
+  const [viewport, setViewport] = useState({
+    latitude: lat,
+    longitude: lon,
+    zoom: 14,
+    bearing: 0,
+    pitch: 0,
+    width: '100%',
+    height: '100%',
+  });
+
+  // Update viewport when lat/lon changes
+  useEffect(() => {
+    setViewport((prevViewport) => ({
+      ...prevViewport,
+      latitude: lat,
+      longitude: lon,
+      transitionDuration: 500, // Smooth transition
+    }));
+  }, [lat, lon]);
+
+  return (
+    <div className="map">
+      <ReactMapGL
+        mapboxAccessToken={API_KEY} // Updated API prop
+        {...viewport}
+        onViewportChange={(newViewport) => setViewport(newViewport)}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+      >
+        <Marker latitude={lat} longitude={lon}>
+          <div className="mark">
+            <RiUserLocationFill size="25px" color="blue" />
+          </div>
+        </Marker>
+      </ReactMapGL>
+    </div>
+  );
+};
+
+export default Map;
+>>>>>>> a551a30 (initial commit)
